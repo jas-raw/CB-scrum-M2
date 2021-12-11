81a7a78fd3c58470440273d01abb1c31543935e4
Jonnathan Sandino <jasandinoa@gmail.com>
2021-12-10 21:40:13 -0500

    feat[CB-7]: crear, listar - se crea metodo crear tareas - se crea metodo listar tareas - se crea modelo tareas - se crea controlador y rutas para tareas

---
+++ back/src/app_bussiness_rules/tasks/use_cases/finalize.js
@@ -0,0 +1,9 @@
+function finalize(task){
+    return function finalize_tasks(user, name){
+        throw new Error('Method not implemented yet')
+    }
+}
+
+module.exports = {
+    finalize
+}
---
+++ back/src/app_bussiness_rules/tasks/use_cases/update.js
@@ -0,0 +1,9 @@
+function update(task){
+    return function update_tasks(user, name){
+        throw new Error('Method not implemented yet')
+    }
+}
+
+module.exports = {
+    update
+}
